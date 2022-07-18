477b7a575739dbb3e53d893684fde4091fb47c9d
alafty <mohamedelelafty@yahoo.com>
2022-07-18 22:54:16 +0200

    Initial Commit

---
+++ Restart/RestartApp.swift
@@ -0,0 +1,17 @@
+//
+//  RestartApp.swift
+//  Restart
+//
+//  Created by Mohamed El-Alafty on 18/07/2022.
+//
+
+import SwiftUI
+
+@main
+struct RestartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
